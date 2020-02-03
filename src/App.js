961d3c2ac03c2b656a04f880443020ff15506a4e
Roshan Mishra <roshanmishrarinem@gmail.com>
2020-02-03 20:22:26 +0530

    Finish Auth components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Navbar from './components/layout/Navbar'
+import Navbar from './components/layout/Navbar';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        <Switch>
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
+          </Switch>
       </div>
     </BrowserRouter>
     
